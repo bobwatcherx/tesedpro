96997a2f09bdf76cc95d03a03494e08cc1791577
root <bobwatcherx@gmail.com>
2022-04-08 09:40:04 +0700

    update code

--- src/controllers/rest/HelloWorldController.ts
+++ src/controllers/rest/HelloWorldController.ts
@@ -1,10 +1,24 @@
-import {Controller} from "@tsed/di";
-import {Get} from "@tsed/schema";
+import {BodyParams, Controller, Get, Post} from "@tsed/common";
+import {Inject} from "@tsed/di";
+import {Groups, Returns, Summary} from "@tsed/schema";
+import {UserModel, UsersRepository} from "@tsed/prisma";
 
-@Controller("/hello-world")
-export class HelloWorldController {
+@Controller("/users")
+export class UsersController {
+  @Inject()
+  protected service: UsersRepository;
+
+  @Post("/")
+  @Summary("Create a new user")
+  @Returns(201, UserModel)
+  async signupUser(@BodyParams() @Groups("creation") user: UserModel): Promise<UserModel> {
+    return this.service.create({data: user});
+  }
+
   @Get("/")
-  get() {
-    return "hello";
+  @Summary("Filter posts by title or content")
+  @Returns(200, Array).Of(UserModel).Description("Return a list of User")
+  getAll() {
+    return this.service.findMany();
   }
 }
